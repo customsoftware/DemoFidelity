7a194d8b65f36a2f16974fe253b280c29ad194e9
Custom Software <kcluff50@gmail.com>
2024-01-22 08:32:11 -0700

    Initial Commit

---
+++ DemoFidelity/DemoFidelityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoFidelityApp.swift
+//  DemoFidelity
+//
+//  Created by Kenneth Cluff on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoFidelityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
