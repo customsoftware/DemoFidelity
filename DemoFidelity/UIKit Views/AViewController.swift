49ff0b6ea23e741ac7450feeb3e39b04f1a823b9
Custom Software <kcluff50@gmail.com>
2024-01-23 09:25:29 -0700

    Comments

--- DemoFidelity/UIKit Views/AViewController.swift
+++ DemoFidelity/UIKit Views/AViewController.swift
@@ -11,13 +11,20 @@ class AViewController: UIViewController {
     
     let wrapper = DataTransporter.shared
     
+    /// I use a UITextView since it can grow in size to enclose the text once it's in read-only mode.
+    /// In the mean time, it is scrollable as text is entered.
     @IBOutlet weak var textView: UITextView!
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Set the value
         textView.text = wrapper.textValue
+        // Set the delegate
         textView.delegate = self
     }
+    
+    /// NOTE: If the view doesn't get dismissed, then code to set the value of the text view can be put in
+    /// the viewWillAppear method of the view controller.
 }
 
 
