e6a7711723a02dac0ed3e2338aba4d9318d0368a
Custom Software <kcluff50@gmail.com>
2024-02-02 14:44:18 -0700

    This shows combine being used for asynchronous processes in a UIKit based view

--- DemoFidelity/UIKit Views/AViewController.swift
+++ DemoFidelity/UIKit Views/AViewController.swift
@@ -6,30 +6,78 @@
 //
 
 import UIKit
+import Combine
 
 class AViewController: UIViewController {
+    private let wrapper = DataTransporter.shared
+   
+    private var cancellables: Set<AnyCancellable> = []
     
-    let wrapper = DataTransporter.shared
+    // Publisher for the timer
+    private let dateFormatter = DateFormatter()
+    private let timePublisher = Timer.publish(every: 1, on: .main, in: .default)
+        .autoconnect()
+        .map{ _ in Date() }
     
-    /// I use a UITextView since it can grow in size to enclose the text once it's in read-only mode.
-    /// In the mean time, it is scrollable as text is entered.
-    @IBOutlet weak var textView: UITextView!
-        
+    @IBOutlet weak var sampleLabel: UILabel!
+    @IBOutlet weak var revieverTextView: UITextView!
+    @IBOutlet weak var aTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Set the value
-        textView.text = wrapper.textValue
-        // Set the delegate
-        textView.delegate = self
+        sampleLabel.text = "Watch this! Example of combine updating label"
+        revieverTextView.text = loadSomeText()
+        
+        dateFormatter.dateStyle = .none
+        dateFormatter.timeStyle = .long
+        
+        // Utilizing the textField publisher to do something with the data.
+        aTextField
+            .textPublisher
+            .sink { [weak self] text in
+                // This forces an asynchronous behavior for the demonstration
+                DispatchQueue.global().asyncAfter(deadline: .now() + 5, execute: {
+                    // Now we push this back onto the main thread.
+                    DispatchQueue.main.async {
+                        // Here we do something with the text. In this instance we're handing it back to the DataTransporter class so the text will appear elsewhere in the application.
+                        self?.wrapper.textValue = text
+                        // Here, I'm storing it to this view so the user can see that something is being done while I type.
+                        self?.revieverTextView.text = text
+                    }
+                })
+            }
+            .store(in: &cancellables)
+        
+        aTextField.text = wrapper.textValue
     }
     
-    /// NOTE: If the view doesn't get dismissed, then code to set the value of the text view can be put in
-    /// the viewWillAppear method of the view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // This is the asynchronous example
+        timePublisher
+            .map { self.dateFormatter.string(from: $0) }
+            .sink { [weak self] time in
+                self?.sampleLabel.text = time
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func loadSomeText() -> String {
+        let retValue =
+        """
+I always knew when growing up that I was destined for greatness. My parents had a way of speaking to me that left the inescapable conclusion that a LOT was going to be expected of me.
+"""
+        return retValue
+    }
 }
 
 
-extension AViewController: UITextViewDelegate {
-    func textViewDidChange(_ textView: UITextView) {
-        wrapper.textValue = textView.text
+// A publisher for the textField
+extension UITextField {
+    var textPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: self)
+            .map { ($0.object as? UITextField)?.text ?? "" }
+            .eraseToAnyPublisher()
     }
 }
